cd321526977b2b7f2763d025e8009988ec6b926c
endotnick <endotnick@gmail.com>
2019-02-27 20:24:53 -0800

    add home

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return <nav>
+    <ul>
+      <li><a href='/movies'>Movies</a></li>
+      <li><a href='/login'>Login</a></li>
+      <li><a href='/signup'>Signup</a></li>
+    </ul>
+  </nav>
+}
