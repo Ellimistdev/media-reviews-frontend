a1b652e93b37a69f7498b5024dbf7763db9ef24f
endotnick <endotnick@gmail.com>
2019-03-02 11:55:29 -0800

    rn home to nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Nav from './components/Nav';
 import MoviesContainer from './containers/MoviesContainer'
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <Router>
         <div className='App'>
-          <Route path='/' component={Home} />
+          <Route path='/' component={Nav} />
           <Route path='/movies' component={MoviesContainer} />        
         </div>
       </Router>
