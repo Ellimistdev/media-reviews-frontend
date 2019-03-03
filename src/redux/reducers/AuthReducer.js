a4faf7a77bae0917a1003c22e2ef34b6c5d0ad01
endotnick <endotnick@gmail.com>
2019-03-03 12:55:24 -0800

    add auth reducer

---
+++ src/redux/reducers/AuthReducer.js
@@ -0,0 +1,39 @@
+import * as types from '../../constants/ActionTypes';
+
+const initialState = {
+  authenticated: false,
+  authenticating: false,
+  user: {},
+  token: localStorage.token || null,
+  errors: []
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case types.AUTHENTICATION_REQUEST:
+      return {
+        ...state,
+        authenticating: true
+      }
+    case types.AUTHENTICATION_SUCCESS:
+      return {
+        ...state,
+        authenticated: true,
+        authenticating: false,
+        user: action.user,
+        token: action.token
+      }
+    case types.AUTHENTICATION_FAILURE:
+      return {
+        authenticated: false,
+        authenticating: false,
+        user: {},
+        token: null,
+        errors: action.errors || []
+      }
+    case types.LOGOUT:
+      return initialState;
+    default:
+      return state;
+  }
+}
