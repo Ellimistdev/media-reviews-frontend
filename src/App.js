a6492915e516cb1ae41dfa1a119cd77f6b241b2c
endotnick <endotnick@gmail.com>
2019-03-03 12:57:15 -0800

    add routes for auth forms

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import MoviesContainer from './containers/MoviesContainer'
+import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className='App'>
-          <Route path='/' component={Nav} />
-          <Route path='/movies' component={MoviesContainer} />        
+          <Route path='/login' component={LoginForm} />        
+          <Route path='/signup' component={RegistrationForm} />               
         </div>
       </Router>
     );
