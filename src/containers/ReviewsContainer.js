6218c79eb5dd0ee592d72fd0b9d6c80f6a2e684e
endotnick <endotnick@gmail.com>
2019-03-03 20:51:49 -0800

    update reviews container

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -4,15 +4,18 @@ import ReviewComponent from '../components/ReviewComponent';
 class ReviewsContainer extends Component {
   render() {
     return (
-      <ul className='review-list'>
-      {
-        this.props.reviews.map(review => {
-          return (
-              <ReviewComponent key={review.id} review={review} />
-          )
-        })
-      }        
-      </ul>
+      <React.Fragment>
+        <h2>Reviews!</h2>
+        <ul className='review-list'>
+        {
+          this.props.reviews.map(review => {
+            return (
+                <ReviewComponent key={review.id} review={review} type={this.props.type}/>
+            )
+          })
+        }        
+        </ul>
+      </React.Fragment>
     )
   }  
 }
