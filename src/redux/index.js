070d36b01b0543dfe517ec7126680cf189822cd2
endotnick <endotnick@gmail.com>
2019-03-03 20:51:01 -0800

    add user reducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,11 +2,13 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import auth from './reducers/AuthReducer';
 import media from './reducers/MovieReducer';
+import user from './reducers/UserReducer';
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   auth,
   media,
+  user,
 });
 
 export default createStore(
---
+++ src/redux/reducers/UserReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+  data: {},
+  reviews: [],
+  views: [],
+  errors: []
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_USER_SUCCESS":
+      return {
+        ...state,
+        data: action.user,
+        reviews: action.user.reviews,
+        views: action.user.views,
+      };
+    default:
+      return state;
+  }
+}
