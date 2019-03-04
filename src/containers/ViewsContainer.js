1cc7a381da56c99888c35dfd5b4063901d082111
endotnick <endotnick@gmail.com>
2019-03-03 20:52:27 -0800

    add views container

---
+++ src/containers/ViewsContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ViewComponent from '../components/ViewComponent';
+
+class ViewsContainer extends Component {
+  render() {
+    return (      
+      <React.Fragment>
+        <h2>Views!</h2>
+        <ul className='view-list'>
+        {
+          this.props.views.map(view => {
+            return (
+                <ViewComponent key={view.id} view={view} />
+            )
+          })
+        }        
+        </ul>
+      </React.Fragment>
+    )
+  }  
+}
+
+export default ViewsContainer;
