ca9b040f32404caafdfecaa2b7a1195594cd04f9
endotnick <endotnick@gmail.com>
2019-03-05 19:36:17 -0800

    add rating to review component

--- src/components/ReviewComponent.js
+++ src/components/ReviewComponent.js
@@ -17,12 +17,14 @@ class ReviewComponent extends Component {
         <React.Fragment>
           <p>Movie: <Link to={`/movies/${review.movie_id}`}>{review.movie_title}</Link></p>
           <p>Review: {review.content}</p>
+          <p>Rating: {review.rating}</p> 
         </React.Fragment>
       );
 
     const movieReview = (
         <React.Fragment>
           <p>Review: {review.content}</p> 
+          <p>Rating: {review.rating}</p> 
           <p>Reviewer: <Link to={`/users/${reviewer_id}`}>{reviewer_email}</Link></p> 
         </React.Fragment>
       );
